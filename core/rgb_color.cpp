df36f28a5de03f5910f24b66fbb0380d661fd169
Antonio Mancera <antonio@mancera.me>
2026-03-08 00:13:50 +0100

    added + operator for RGBColor

--- core/rgb_color.cpp
+++ core/rgb_color.cpp
@@ -1,3 +1,7 @@
 #include "rgb_color.h"
 
-RGBColor::RGBColor(float r, float g, float b): red(r), green(g), blue(b) {}
+RGBColor::RGBColor(float r, float g, float b) : red(r), green(g), blue(b) {}
+
+RGBColor RGBColor::operator+(const RGBColor &color) const {
+  return RGBColor(color.red + red, color.green + green, color.blue + blue);
+}
