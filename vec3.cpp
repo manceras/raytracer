5b3accca6928f1dfeb81549ff3822ed2fc790e38
Antonio Mancera <antonio@mancera.me>
2026-03-07 17:23:53 +0100

    fix: added const and changed * to pass by ref

--- vec3.cpp
+++ vec3.cpp
@@ -4,9 +4,7 @@ using namespace std;
 
 class Vec3 {
 public:
-  float x;
-  float y;
-  float z;
+	float x, y, z;
 
   Vec3(float x, float y, float z) {
     this->x = x;
@@ -14,13 +12,13 @@ public:
     this->z = z;
   }
 
-  Vec3 operator+(Vec3 &v) { return Vec3(x + v.x, y + v.y, z + v.z); }
+  Vec3 operator+(const Vec3 &v) { return Vec3(x + v.x, y + v.y, z + v.z); }
 
-  Vec3 operator-(Vec3 &v) { return Vec3(x - v.x, y - v.y, z - v.z); }
+  Vec3 operator-(const Vec3 &v) { return Vec3(x - v.x, y - v.y, z - v.z); }
 
   Vec3 operator*(float n) { return Vec3(x * n, y * n, z * n); }
 
-  float operator*(Vec3 *v) { return x * v->x + y * v->y + z * v->z; }
+  float operator*(const Vec3 &v) { return x * v.x + y * v.y + z * v.z; }
 
   Vec3 normalize() {
     float mod = sqrt(x*x + y*y + z*z);
