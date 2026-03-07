c3f78fdde2767c482a28dd66f74569f04e4e6f90
Antonio Mancera <antonio@mancera.me>
2026-03-07 17:24:17 +0100

    feat: created vec3.h file

---
+++ Vec3.h
@@ -0,0 +1,21 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+class Vec3 {
+public:
+  float x, y, z;
+
+  Vec3(float x, float y, float z);
+
+  Vec3 operator+(const Vec3 &v);
+
+  Vec3 operator-(const Vec3 &v);
+
+  Vec3 operator*(float n);
+
+  float operator*(const Vec3 &v);
+
+  Vec3 normalize();
+};
+
+#endif
