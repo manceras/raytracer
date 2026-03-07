2ecfd9e6bee20fc5247ad9c1dc939a4ac4568435
Antonio Mancera <antonio@mancera.me>
2026-03-07 22:58:06 +0100

    swaped ifs because I was returning the furthest point

--- geometries/sphere.cpp
+++ geometries/sphere.cpp
@@ -16,9 +16,9 @@ float Sphere::collides(Ray &ray) {
   float t1 = (-b + discriminant) / (2 * a);
   float t2 = (-b - discriminant) / (2 * a);
 
-  if (t1 > 0)
-    return t1;
   if (t2 > 0)
     return t2;
+  if (t1 > 0)
+    return t1;
   return -1;
 }
