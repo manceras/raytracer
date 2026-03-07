8725743ec9fbe28af35da9c3f9f5a91a3bef5f03
Antonio Mancera <antonio@mancera.me>
2026-03-07 23:50:25 +0100

    added inverse of square law to make distance to light source affect brightness

--- light.cpp
+++ light.cpp
@@ -4,8 +4,9 @@ Light::Light(Vec3 position, float intensity)
     : position(position), intensity(intensity) {}
 
 float Light::multiplier_for_point(Vec3 normal, Vec3 point) {
-	Vec3 impact_to_light = (position - point).normalize();
-  float dot = normal * impact_to_light;
+	Vec3 impact_to_light = position - point;
+  float dot = normal * impact_to_light.normalize();
+	float distance = impact_to_light.modulus();
 	if (dot < 0) return 0;
-  return intensity * dot;
+  return intensity * dot / (distance * distance);
 }
