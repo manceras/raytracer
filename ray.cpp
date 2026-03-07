77095cb32f88f37eb00d85c9abab7c3248ebb920
Antonio Mancera <antonio@mancera.me>
2026-03-07 22:01:18 +0100

    added ray constructor

--- ray.cpp
+++ ray.cpp
@@ -1,10 +1,11 @@
 #include "vec3.h"
 
 class Ray {
-	public:
-		Vec3 point, direction;
+public:
+  Vec3 point, direction;
 
-		Vec3 at(float t) {
-			return point + t * direction;
-		}
+  Ray(const Vec3 &point, const Vec3 &direction)
+      : point(point), direction(direction) {}
+
+  Vec3 at(float t) { return point + t * direction; }
 };
--- ray.h
+++ ray.h
@@ -3,5 +3,6 @@
 class Ray {
 	public:
 		Vec3 point, direction;
+		Ray(const Vec3 &point, const Vec3 &direction);
 		Vec3 at(float t);
 };
