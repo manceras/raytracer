1f8b2fb7209cd62810d0b1377da6b2aab87c5b41
Antonio Mancera <antonio@mancera.me>
2026-03-07 21:47:58 +0100

    added sphere geometry

---
+++ geometries/sphere.cpp
@@ -0,0 +1,26 @@
+#include "../ray.h"
+#include "../vec3.h"
+#include <cmath>
+
+class Spehere {
+private:
+  float radius;
+  Vec3 center;
+
+public:
+  float collides(Ray &ray) {
+    float a = ray.direction * ray.direction;
+    Vec3 l = ray.point - center;
+    float b = 2 * (ray.direction * l);
+    float c = (l * l) - radius * radius;
+
+    float discriminant = sqrt(b * b - 4 * a * c);
+
+    float t1 = (-b + discriminant) / (2 * a);
+    float t2 = (-b - discriminant) / (2 * a);
+
+		if (t1 > 0) return t1;
+		if (t2 > 0) return t2;
+		return -1;
+  }
+};
