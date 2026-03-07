6de382d2a035c60e8c9ae4fb09f923621c4c4914
Antonio Mancera <antonio@mancera.me>
2026-03-07 17:17:34 +0100

    feat: vec3 implementation

---
+++ vec3.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+
+using namespace std;
+
+class Vec3 {
+public:
+  float x;
+  float y;
+  float z;
+
+  Vec3(float x, float y, float z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+  }
+
+  Vec3 operator+(Vec3 &v) { return Vec3(x + v.x, y + v.y, z + v.z); }
+
+  Vec3 operator-(Vec3 &v) { return Vec3(x - v.x, y - v.y, z - v.z); }
+
+  Vec3 operator*(float n) { return Vec3(x * n, y * n, z * n); }
+
+  float operator*(Vec3 *v) { return x * v->x + y * v->y + z * v->z; }
+
+  Vec3 normalize() {
+    float mod = sqrt(x*x + y*y + z*z);
+    return Vec3(x / mod, y / mod, z / mod);
+  }
+};
