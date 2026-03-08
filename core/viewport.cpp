41244ea4a202da6752e20b25a4b7210cb42de99a
Antonio Mancera <antonio@mancera.me>
2026-03-08 16:08:04 +0100

    feature: now you can move the camera

--- core/viewport.cpp
+++ core/viewport.cpp
@@ -2,7 +2,11 @@
 #include "ray.h"
 #include "vec3.h"
 
-Viewport::Viewport(int width, int height) : width(width), height(height) {}
+Viewport::Viewport(int width, int height, Vec3 origin, float focal_length,
+                   Vec3 look_at)
+    : width(width), height(height), origin(origin), focal_length(focal_length),
+      direction((look_at - origin).normalize()),
+      right(direction.cross(Vec3(0, 1, 0))), up(direction.cross(right)) {}
 
 Ray Viewport::rayForPx(int column, int row) {
   float normalized_i = (float)column / width;
@@ -11,13 +15,14 @@ Ray Viewport::rayForPx(int column, int row) {
   float aspect_ratio = (float)width / height;
   float pixel_aspect = 1;
 
+  Vec3 forward = direction;
+  Vec3 right = direction.cross(Vec3(0, 1, 0));
+  Vec3 up = forward.cross(right);
+
   float x = (2.0 * normalized_i - 1.0) * aspect_ratio * pixel_aspect;
   float y = 1.0 - 2.0 * normalized_j;
 
-  Vec3 point = Vec3(x, y, 1);
+  Vec3 point = origin + forward * focal_length + right * x + up * y;
 
-  Vec3 origin = Vec3(0, 0, 0);
-  Vec3 direction = point.normalize();
-
-  return Ray(origin, direction);
+  return Ray(origin, (point - origin).normalize());
 }
--- core/viewport.h
+++ core/viewport.h
@@ -6,11 +6,17 @@
 
 class Viewport {
 private:
-  float distance = 1;
+  Vec3 origin;
+  Vec3 direction;
+  float focal_length;
   int width, height;
 
+	Vec3 up;
+	Vec3 right;
+
 public:
-	Viewport(int width, int height);
+  Viewport(int width, int height, Vec3 origin, float focal_length,
+           Vec3 look_at);
   Ray rayForPx(int column, int row);
 };
 
