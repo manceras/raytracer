9ad9849883f5fbbc5f0d8a4bd1036420ef077a75
Antonio Mancera <antonio@mancera.me>
2026-03-14 20:21:16 +0100

    implement Monte Carlo path tracing with diffuse and specular bounces

--- core/trace.cpp
+++ core/trace.cpp
@@ -1,4 +1,5 @@
 #include "trace.h"
+#include "../utils/random_dir.h"
 #include "bvh_node.h"
 #include "ray.h"
 #include "rgb_color.h"
@@ -6,32 +7,41 @@
 
 RGBColor trace(Ray ray, vector<Light> lights, const BVHNode &bvh, int depth) {
 
-	optional<HitFace> hit_face_opt = bvh.hit_face(ray);
+  optional<HitFace> hit_face_opt = bvh.hit_face(ray);
 
   if (!hit_face_opt)
     return RGBColor(0, 0, 0);
 
-	HitFace hit_face = hit_face_opt.value();
-	float distance = hit_face.hit.t;
-	Vec3 normal = hit_face.face.normal_at(hit_face.hit);
-	Vec3 hit_point = ray.at(distance);
-
+  HitFace hit_face = hit_face_opt.value();
+  float distance = hit_face.hit.t;
+  Vec3 normal = hit_face.face.normal_at(hit_face.hit);
+  Vec3 hit_point = ray.at(distance);
   Vec3 impact_point = ray.at(distance);
+
+  if (normal * ray.direction < 0)
+    normal = normal * -1;
+
   RGBColor multiplier = RGBColor(0, 0, 0);
   for (const Light light : lights) {
     multiplier = multiplier + light.multiplier_for_point(normal, impact_point);
   }
 
-  RGBColor difuse_color =
-      RGBColor(multiplier.red, multiplier.green, multiplier.blue);
+  if (depth <= 0) {
+    return multiplier;
+  }
 
-  if (depth <= 0)
-    return difuse_color;
+  Vec3 difused_direction = random_direction();
+
+  if (difused_direction * normal < 0)
+    difused_direction = -1 * difused_direction;
+
+  Ray difused_ray = Ray(hit_point + normal * 0.001, difused_direction);
 
   Vec3 reflected_direction =
       ray.direction - 2 * (ray.direction * normal) * normal;
   Ray reflected_ray = Ray(hit_point + normal * 0.001, reflected_direction);
-  return hit_face.face.get_material().kd * difuse_color +
+
+  return hit_face.face.get_material().kd * trace(difused_ray, lights, bvh, depth -1) + multiplier +
          hit_face.face.get_material().ks *
              trace(reflected_ray, lights, bvh, depth - 1);
 }
